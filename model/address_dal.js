50d47e951124d19dc348328c691c9fd1c74b0143
Adrian Smith <adrian@smithdev.io>
2016-12-08 00:17:17 +0000

    Bugfix on addresses

--- model/address_dal.js
+++ model/address_dal.js
@@ -11,3 +11,35 @@ exports.getAll = function(callback) {
         callback(err, result);
     });
 };
+
+exports.getById = function(address_id, callback) {
+    var query = 'SELECT * FROM address WHERE address_id = ?';
+    var queryData = [address_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
+
+exports.insert = function(params, callback) {
+    var query = 'INSERT INTO address (street, zip_code) VALUES (?, ?)';
+
+    // the question marks in the sql query above will be replaced by the values of the
+    // the data in queryData
+    var queryData = [params.street, params.zip_code];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+}
+
+exports.delete = function(address_id, callback) {
+    var query = 'DELETE FROM address WHERE address_id = ?';
+    var queryData = [address_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+};
